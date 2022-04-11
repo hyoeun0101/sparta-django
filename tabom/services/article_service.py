a8143265b2a5d600c20970ef24286b375d92f989
hyoeun0101 <kimhyoeun0101@gmail.com>
2022-04-11 18:01:23 +0900

    user prefetch

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,5 +1,24 @@
-from tabom.models import Article
+from typing import List
+
+from django.core.paginator import Page, Paginator
+from django.db.models import Prefetch, QuerySet
+
+from tabom.models import Article, Like
 
 
 def get_an_article(article_id: int) -> Article:
     return Article.objects.filter(id=article_id).get()
+
+
+def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]:
+    return (
+        Article.objects.order_by("-id")
+        .prefetch_related("like_set")
+        .prefetch_related(Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes"))[
+            offset : offset + limit
+        ]
+    )
+
+
+# def get_article_list(offset:int, limit: int) -> QuerySet[Article]:
+#     return Article.objects.order_by("-id").prefetch_related("like_set")[offset: offset+limit]
--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -1,7 +1,11 @@
+from django.db import connection
 from django.test import TestCase
+from django.test.utils import CaptureQueriesContext
 
+from tabom.models import User
 from tabom.models.article import Article
-from tabom.services.article_service import get_an_article
+from tabom.services.article_service import get_an_article, get_article_list
+from tabom.services.like_service import do_like
 
 
 class TestArticleService(TestCase):
@@ -24,3 +28,36 @@ class TestArticleService(TestCase):
         # Expect
         with self.assertRaises(Article.DoesNotExist):
             get_an_article(invalid_article_id)
+
+    def test_get_article_list_should_prefetch_like(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        articles = [Article.objects.create(title=f"{i}") for i in range(1, 21)]
+        # 최신 게시글에만 좋아요해주기
+        like = do_like(user.id, articles[-1].id)
+
+        # When
+        # with CaptureQueriesContext(connection) as ctx:
+        with self.assertNumQueries(3):
+            # article 조회 한번, like 조회 한번
+            result_articles = get_article_list(user.id, 0, 10)
+            result_counts = [a.like_set.count() for a in result_articles]
+
+        # Then
+        self.assertEqual(10, len(result_articles))
+        self.assertEqual(1, result_counts[0])
+        self.assertEqual([a.id for a in reversed(articles[10:21])], [a.id for a in result_articles])
+
+    def test_get_article_list_should_contain_my_like_when_like_exists(self) -> None:
+        # Given
+        user = User.objects.create(name="test_user")
+        article = Article.objects.create(title="article1")
+        like = do_like(user.id, article.id)
+        Article.objects.create(title="article2")
+
+        # When
+        articles = get_article_list(user.id, 0, 10)
+
+        # Then
+        self.assertEqual(like.id, articles[1].my_likes[0].id)
+        self.assertEqual(0, len(articles[0].my_likes))
